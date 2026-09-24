a64c9b3ba1021c6b8925fdc0e841dd99cbbbbf6d
Muhammad Nur Ichsan Putra Adnan <putraadnan@Muhammads-MacBook-Air.local>
2026-09-24 15:53:10 +0800

    inisialisasi kelas Transaction dengan constructor property

---
+++ Tugas 5/Transaction.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+
+class Transaction
+{
+    public function __construct(
+        private string $id,
+        private string $type,
+        private float $amount
+    ) {}
+
+    public function getId(): string { return $this->id; }
+    public function getType(): string { return $this->type; }
+    public function getAmount(): float { return $this->amount; }
+}
